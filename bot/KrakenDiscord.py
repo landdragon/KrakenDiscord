8ef7e6be91895afba66cc8373b0d4b400151c538
landdragon <guirado.maxime@gmail.com>
2021-05-14 15:01:37 +0200

    rework

--- bot/KrakenDiscord.py
+++ bot/KrakenDiscord.py
@@ -84,26 +84,26 @@ async def addCash(ctx: commands.Context, quantity: int):
     if ctx.channel.name != CHANNEL_WORK:
         return
     if GetCashFromDataBase(ctx.author.name) == None:
-        await InsertCurrencyToDataBase(ctx.author.name, quantity, "eur")
+        InsertCurrencyToDataBase(ctx.author.name, quantity, "eur")
     else:
-        await UpdateCurrencyToDataBase(ctx.author.name, quantity, "eur")
+        UpdateCurrencyToDataBase(ctx.author.name, quantity, "eur")
     await ctx.send("Done")
 
 
-async def InsertCurrencyToDataBase(authorName: str, quantity: int, currency: str):
+def InsertCurrencyToDataBase(authorName: str, quantity: int, currency: str):
     sql = "INSERT INTO \"Wallets\"(	\"UserName\", \"Currency\", \"Quantity\", \"createdAt\", \"updatedAt\") VALUES('" + authorName + \
         "', '"+currency+"', "+quantity+", '"+datetime.now().strftime("%d/%m/%Y %H:%M:%S") + \
         "', '"+datetime.now().strftime("%d/%m/%Y %H:%M:%S")+"')"
     cur = conn.cursor()
-    await cur.execute(sql)
+    cur.execute(sql)
     cur.close()
 
 
-async def UpdateCurrencyToDataBase(authorName: str, quantity: int, currency: str):
+def UpdateCurrencyToDataBase(authorName: str, quantity: int, currency: str):
     sql = "update \"Wallets\" set \"Quantity\" = " + quantity + " and  \"updatedAt\" = '"+datetime.now().strftime("%d/%m/%Y %H:%M:%S")+"' WHERE \"UserName\" = '" + authorName + \
         "' and \"Currency\" = '" + currency + "'"
     cur = conn.cursor()
-    await cur.execute(sql)
+    cur.execute(sql)
     cur.close()
 
 
@@ -112,7 +112,7 @@ async def getCash(ctx: commands.Context):
     try:
         if ctx.channel.name != CHANNEL_WORK:
             return
-        records = await GetCashFromDataBase(ctx.author.name, "eur")
+        records = GetCashFromDataBase(ctx.author.name, "eur")
         print(records)
         if records == None:
             await ctx.send(0)
@@ -122,11 +122,11 @@ async def getCash(ctx: commands.Context):
         print("error : " + ValueError)
 
 
-async def GetCashFromDataBase(authorName: str, currency: str):
+def GetCashFromDataBase(authorName: str, currency: str):
     sql = "SELECT \"Quantity\" FROM \"Wallets\" WHERE \"UserName\" = '" + authorName + \
         "' and \"Currency\" = '"+currency+"'"
     cur = conn.cursor()
-    await cur.execute(sql)
+    cur.execute(sql)
     records = cur.fetchone()
     cur.close()
     return records
